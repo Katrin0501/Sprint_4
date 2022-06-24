94973d8d9dce974a8f59b989cdc55e199142f035
Ekaterina Krylova <Kkatrin0501@yandex.ru>
2022-06-24 13:14:09 +0300

    Внесла правки в класс Praktikum

--- src/main/java/ru/yandex/praktikum/Praktikum.java
+++ src/main/java/ru/yandex/praktikum/Praktikum.java
@@ -1,15 +1,20 @@
 package ru.yandex.praktikum;
 
+import java.util.Scanner;
+
 public class Praktikum {
 
     public static void main(String[] args) {
-        String fullName = "Имя фамилия";
 
-        Account account = new Account(fullName);
+        Scanner in = new Scanner(System.in);
+        String name = in.nextLine();
+        Account account = new Account(name);
         account.checkNameToEmboss();
-        System.out.println(fullName);
+
     }
-}
+
+    }
+
         /*
              В этом методе заложи логику работы с классом Account.
              Нужно создать экземпляр класса Account: в качестве аргумента передать тестируемое имя
